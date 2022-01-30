20d43b155022064b3a242b9a7966f7e56974d2ac
Marta Żak <martysik19@gmail.com>
2022-01-30 20:19:56 +0100

    added task 2 solution

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listMarkup = [];
+
+const listRef = document.querySelector('#ingredients');
+
+const makeIngredientsListMarkup = function (ingredients) {
+  return ingredients.forEach(ingredient => {
+    const listEl = document.createElement('li');
+
+    listEl.textContent = `${ingredient}`;
+    listEl.classList.add('item');
+
+    listMarkup.push(listEl);
+  });
+};
+
+makeIngredientsListMarkup(ingredients);
+
+listRef.append(...listMarkup);
