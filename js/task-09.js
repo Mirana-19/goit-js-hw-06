3739dd06b80eba7076d158f62e0a6c4aaac34b36
Marta Żak <martysik19@gmail.com>
2022-01-31 23:54:48 +0100

    task 9 solution added

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorNameRef = document.querySelector('.color');
+const changeColorBtnRef = document.querySelector('.change-color');
+
+changeColorBtnRef.addEventListener('click', changeBodyColor);
+
+function changeBodyColor(event) {
+  const currentColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = currentColor;
+  colorNameRef.textContent = currentColor;
+}
