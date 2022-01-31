3aaab939618d5610919fc5415cb8911aab32d0cd
Marta Żak <martysik19@gmail.com>
2022-01-31 23:36:21 +0100

    task 8 solution added

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+  email: document.querySelector('[type="email"]'),
+  password: document.querySelector('[type="password"]'),
+  submit: document.querySelector('button'),
+};
 
+refs.form.addEventListener('submit', checkValidation);
+
+function checkValidation(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Пожалуйста, заполните все поля!');
+  } else {
+    console.log({
+      email: email.value,
+      password: password.value,
+    });
+    event.currentTarget.reset();
+  }
+}
